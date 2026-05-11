c90a0affab9e378dc52b18df7b0a25601d18309f
Your Name <you@example.com>
2026-05-12 00:24:41 +0800

    create main

--- lib/src/main/java/me/abcdd/numw.java
+++ lib/src/main/java/me/abcdd/numw.java
@@ -1,7 +1,39 @@
 package me.abcdd;
 
-public class numw {
-    public numw{
+public class numw extends Number {
+    public numw() {
+    
     }
     
+    
+    public int intValue(){
+    
+        return 0;
+    }
+
+
+    public long longValue(){
+    
+        return 0L;
+    }
+
+
+    public float floatValue(){
+        return (float)doubleValue();
+    }
+
+
+    public double doubleValue(){
+        return 0.0;
+    }
+
+
+    public byte byteValue() {
+        return (byte)intValue();
+    }
+
+
+    public short shortValue() {
+        return (short)intValue();
+    }
 }
---
+++ lib/src/main/java/me/abcdd/types.java
@@ -0,0 +1,5 @@
+package me.abcdd;
+
+public enum types {
+INT,LONG,BIGINTEGER,FLOAT,DOUBLE,BIGDECIMAL
+}
