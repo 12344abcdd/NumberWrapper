390271fd299a8656c128d80395d1c3af7336edf4
Your Name <you@example.com>
2026-06-04 12:18:14 +0800

    -

--- lib/src/main/java/me/abcdd/Getable.java
+++ lib/src/main/java/me/abcdd/Getable.java
@@ -12,26 +12,28 @@ public interface Getable{
   public java.math.BigInteger BigIntegerValue();
   public java.math.BigDecimal BigDecimalValue();
   
-  public static T Value()<T extands Number> {
-        switch (type){
-            case types.BYTE:
-                return byte_value;
-            case types.SHORT:
-                return (byte)short_value;
-            case types.INT:
-                return (byte)int_value;
-            case types.LONG:
-                return (byte)long_value;
-            case types.FLOAT:
-                return (byte)float_value;
-            case types.DOUBLE:
-                return (byte)double_value;
-            case types.BIGINTEGER:
-                return BigInteger_value.byteValue();
-            case types.BIGDECIMAL:
-                return BigDecimal_value.byteValue();
-            default:
-                throw new RuntimeException();
-            }
+  @SuppressWarnings("unchecked")
+public static <T extends Number> T getValue(numw numwObj) {
+    switch (numwObj.type){
+        case types.BYTE:
+            return (T) Byte.valueOf(numwObj.byte_value);
+        case types.SHORT:
+            return (T) Short.valueOf(numwObj.short_value);
+        case types.INT:
+            return (T) Integer.valueOf(numwObj.int_value);
+        case types.LONG:
+            return (T) Long.valueOf(numwObj.long_value);
+        case types.FLOAT:
+            return (T) Float.valueOf(numwObj.float_value);
+        case types.DOUBLE:
+            return (T) Double.valueOf(numwObj.double_value);
+        case types.BIGINTEGER:
+            // BigInteger → Number子类，直接强转
+            return (T) numwObj.BigInteger_value;
+        case types.BIGDECIMAL:
+            return (T) numwObj.BigDecimal_value;
+        default:
+            throw new RuntimeException("不支持的数字类型");
     }
+}
 }
