7c2cbe61fb1817e9fca337a0782061e52ba27f94
Your Name <you@example.com>
2026-06-04 09:58:46 +0800

    -

--- lib/src/main/java/me/abcdd/Getable.java
+++ lib/src/main/java/me/abcdd/Getable.java
@@ -11,4 +11,27 @@ public interface Getable{
   public double doubleValue();
   public java.math.BigInteger BigIntegerValue();
   public java.math.BigDecimal BigDecimalValue();
+  
+  public static T Value()<T extands Number> {
+        switch (type){
+            case types.BYTE:
+                return byte_value;
+            case types.SHORT:
+                return (byte)short_value;
+            case types.INT:
+                return (byte)int_value;
+            case types.LONG:
+                return (byte)long_value;
+            case types.FLOAT:
+                return (byte)float_value;
+            case types.DOUBLE:
+                return (byte)double_value;
+            case types.BIGINTEGER:
+                return BigInteger_value.byteValue();
+            case types.BIGDECIMAL:
+                return BigDecimal_value.byteValue();
+            default:
+                throw new RuntimeException();
+            }
+    }
 }
