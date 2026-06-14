604b3428067fcc6dd86bdb1d8da7601e5a678512
Your Name <you@example.com>
2026-06-14 23:29:09 +0800

    -

--- lib/src/main/java/me/abcdd/Types.java
+++ lib/src/main/java/me/abcdd/Types.java
@@ -3,6 +3,40 @@ package me.abcdd;
 public enum Types {
 BYTE,SHORT,INT,LONG,BIGINTEGER,FLOAT,DOUBLE,BIGDECIMAL;
     
+    public static boolean isIntegerType(Types type){
+        if (type==null){
+            return false;
+        }
+        if(type.ordinal()<=4){
+            return true;
+        }
+        return false;
+    }
+    
+    public static boolean isInteger(numW numwObj){
+        if (numwObj==null){
+            return false;
+        }
+        return isIntegerType(numwObj.type);
+    }
+    
+    public static boolean isDecimalType(Types type){
+        if (type==null){
+            return false;
+        }
+        if(type.ordinal()>=5){
+            return true;
+        }
+        return false;
+    }
+    
+    public static boolean isDecimal(numW numwObj){
+        if (numwObj==null){
+            return false;
+        }
+        return isDecimalType(numwObj.type);
+    }
+    
     public static Types cast(Types t1, Types t2) {
     // 任意一方是BigDecimal → BigDecimal
     if(t1==BIGDECIMAL || t2==BIGDECIMAL) return BIGDECIMAL;
@@ -14,5 +48,5 @@ BYTE,SHORT,INT,LONG,BIGINTEGER,FLOAT,DOUBLE,BIGDECIMAL;
     if(t1==BIGINTEGER || t2==BIGINTEGER) return BIGINTEGER;
     // 纯整数按ordinal升级
     return t1.ordinal()>=t2.ordinal()?t1:t2;
-}
+    }
 }
